78abdfce1d90465b85ab092406fc2f27d56a23b7
thariq <thariq.khalid@gmail.com>
2018-11-06 17:39:31 +0300

    Including net.py for commit

--- src/net.py
+++ src/net.py
@@ -1,9 +1,12 @@
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.preprocessing.image import ImageDataGenerator
+
+NUM_CLASSES = 196
 
 model = Sequential()
-model.add(Conv2D(32, (3, 3), input_shape=(3, 150, 150)))
+model.add(Conv2D(32, (3, 3), input_shape=(150, 150, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
@@ -19,7 +22,7 @@ model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
 model.add(Dense(64))
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
-model.add(Dense(1))
+model.add(Dense(NUM_CLASSES))
 model.add(Activation('sigmoid'))
 
 model.compile(loss='binary_crossentropy',
@@ -43,22 +46,29 @@ test_datagen = ImageDataGenerator(rescale=1./255)
 # subfolers of 'data/train', and indefinitely generate
 # batches of augmented image data
 train_generator = train_datagen.flow_from_directory(
-        'data/train',  # this is the target directory
+        '../data/train',  # this is the target directory
         target_size=(150, 150),  # all images will be resized to 150x150
         batch_size=batch_size,
-        class_mode='binary')  # since we use binary_crossentropy loss, we need binary labels
+        class_mode='categorical')  # since we use binary_crossentropy loss, we need binary labels
 
 # this is a similar generator, for validation data
 validation_generator = test_datagen.flow_from_directory(
-        'data/validation',
+        '../data/val',
         target_size=(150, 150),
         batch_size=batch_size,
-        class_mode='binary')
+        class_mode='categorical')
 
 model.fit_generator(
         train_generator,
         steps_per_epoch=2000 // batch_size,
         epochs=50,
+        verbose=1,
+        workers=4,
         validation_data=validation_generator,
         validation_steps=800 // batch_size)
+
 model.save_weights('first_try.h5')  # always save your weights after training or during training
+
+with open('model_architecture.json','w') as f:
+        f.write(model.to_json())
+
